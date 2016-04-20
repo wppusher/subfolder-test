86038928ed8ab8d0e683913cdb1c1bbfdb2aa7fe
Peter Suhm <peter@suhm.dk>
2016-04-20 10:57:23 +0200

    initial commit

---
+++ trunk/cool-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Cool Plugin
+ * Plugin URI: https://github.com/wppusher/Cool-Plugin
+ * Description: OMG this plugin is so cool.
+ * Version: 1.0.0
+ * Author: WP Pusher
+ * Author URI: http://wppusher.com
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
